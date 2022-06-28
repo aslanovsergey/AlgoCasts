ea796772fffd1e6f493bba9dfc1c74791f695401
Sergey <aslanovsergey@gmail.com>
2022-06-28 16:16:28 +0300

    resolve palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    var reversedStr = [...str].reverse().join("");
+    return reversedStr == str;
+}
 
 module.exports = palindrome;
