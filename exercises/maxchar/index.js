a48e8b4621212b3e92aed667e4500cd58d165898
Sergey <aslanovsergey@gmail.com>
2022-06-28 17:04:59 +0300

    solve maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,14 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    var map = {};
+
+    for (const char of str) {
+        map[char] = (map[char] ? map[char] : 0) + 1
+    }
+
+    return Object.entries(map).sort((a, b) => b[1] - a[1])[0][0]
+}
 
 module.exports = maxChar;
