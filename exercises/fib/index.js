294d0c5b3f2c27e812bdc33e862b761f9a46d688
Sergey <aslanovsergey@gmail.com>
2022-06-30 15:18:59 +0300

    solve fibonacci with memoization (cache)

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -20,16 +20,32 @@
 //   return array[n];
 // }
 
-function fib(n, array = [0, 1]) {
-    if(array.length < n + 1) {
-        const a = array[array.length - 1];
-        const b = array[array.length - 2];
-        array.push(a + b);
+// function fib(n, array = [0, 1]) {
+//   if (array.length <= n) {
+//     const a = array[array.length - 1];
+//     const b = array[array.length - 2];
+//     array.push(a + b);
 
-        fib(n, array);
-    }
-  
-    return array[n];
+//     fib(n, array);
+//   }
+
+//   return array[n];
+// }
+
+const cache = {};
+
+function fib(n) {
+
+  if (n < 2) {
+    return n;
   }
+
+  if (!cache[n]) {
+    const value = fib(n - 1) + fib(n - 2);
+    cache[n] = value;
+  }
+
+  return cache[n];
+}
 
 module.exports = fib;
