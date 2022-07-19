648e99635fcd649129e2dbf2be81003ca9cbc51e
Sergey <aslanovsergey@gmail.com>
2022-07-19 13:02:00 +0300

    solve validate binary search tree

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,23 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if ((min != null && node.data <= min) || (max != null && node.data >= max)) {
+    return false;
+  }
+
+  let leftSideResult = true;
+  let rightSideResult = true;
+
+  if (node.left) {
+    leftSideResult = validate(node.left, min, node.data);
+  }
+
+  if (node.right) {
+    rightSideResult = validate(node.right, node.data, max);
+  }
+
+  return !leftSideResult || !rightSideResult ? false : true;
+}
 
 module.exports = validate;
