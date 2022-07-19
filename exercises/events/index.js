a494c9344c5c57f51743605c757063e5f0b670fd
Sergey <aslanovsergey@gmail.com>
2022-07-19 13:02:56 +0300

    solve events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,29 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.handlers = new Map();
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    this.handlers[eventName] = [...(this.handlers[eventName] || []), callback];
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    const eventHandlers = this.handlers[eventName];
+    if (eventHandlers) {
+      eventHandlers.forEach((e) => e());
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.handlers[eventName] = undefined
+  }
 }
 
 module.exports = Events;
